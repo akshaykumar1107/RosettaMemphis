1ad648b4037c8ddc6ccdf17a2995b69b7e547f1d
Akshay Kumar <akshaykumarcnr@gmail.com>
2026-07-19 16:03:06 +0530

    Added Redis Configuration.

---
+++ src/main/java/com/rosetta/app/configuration/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.rosetta.app.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisConfig
+{
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory)
+    {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofDays(1))//cache expiry.
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))//store key as string instead of binary.
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<>(Object.class)))//same as above.
+                .disableCachingNullValues();//do not cache null values. Fetch from source until value is not null. Or null will be returned, even if source has non-null value.
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
