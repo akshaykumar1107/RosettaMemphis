9bac57b05f277634260711d289b00c09836b6087
Akshay Kumar <akshaykumarcnr@gmail.com>
2026-08-02 22:41:59 +0530

    Added interceptor configuration.

--- src/main/java/com/rosetta/app/interceptor/configuration/InterceptorConfiguration.java
+++ src/main/java/com/rosetta/app/interceptor/configuration/InterceptorConfiguration.java
@@ -1,5 +1,6 @@
 package com.rosetta.app.interceptor.configuration;
 
+import com.rosetta.app.interceptor.AuthorizationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class InterceptorConfiguration implements WebMvcConfigurer
 {
+    private final AuthorizationInterceptor authorizationInterceptor;
+
+    public InterceptorConfiguration(AuthorizationInterceptor authorizationInterceptor)
+    {
+        this.authorizationInterceptor = authorizationInterceptor;
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry)
     {
-
+        registry.addInterceptor(authorizationInterceptor).addPathPatterns("/api/**");
     }
 }
