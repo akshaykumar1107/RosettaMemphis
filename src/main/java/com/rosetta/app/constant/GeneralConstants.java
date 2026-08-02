25720edfec5b08b656df2d089c8e5543fe489970
Akshay Kumar <akshaykumarcnr@gmail.com>
2026-08-02 22:41:39 +0530

    Added API_KEYS constant.

--- src/main/java/com/rosetta/app/constant/GeneralConstants.java
+++ src/main/java/com/rosetta/app/constant/GeneralConstants.java
@@ -6,4 +6,5 @@ public class GeneralConstants
     public static final String API_KEY = "API-KEY";
     public static final String PLAN = "plan";
     public static final String USER_ID = "user_id";
+    public static final String API_KEYS = "api_keys";
 }
