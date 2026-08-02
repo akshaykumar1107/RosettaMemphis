4dcbf2cd5376665163a8fad4e0bf10f564fbeed8
Akshay Kumar <akshaykumarcnr@gmail.com>
2026-08-02 22:38:24 +0530

    Added api key authentication

--- src/main/java/com/rosetta/app/interceptor/AuthorizationInterceptor.java
+++ src/main/java/com/rosetta/app/interceptor/AuthorizationInterceptor.java
@@ -1,14 +1,54 @@
 package com.rosetta.app.interceptor;
 
+import org.springframework.beans.factory.annotation.Value;
+import com.rosetta.app.constant.APIResponse;
+import com.rosetta.app.constant.GeneralConstants;
+import com.rosetta.app.entity.ApiKey;
+import com.rosetta.app.exception.ResponseException;
+import com.rosetta.app.repository.ApiKeyRepository;
+import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.util.Optional;
+
+@Component
 public class AuthorizationInterceptor implements HandlerInterceptor
 {
+    private final ApiKeyRepository apiKeyRepository;
+
+    @Value("${app.admin.apikey}")//inject a value from application.properties
+    private String adminApiKey;
+
+    public AuthorizationInterceptor(ApiKeyRepository apiKeyRepository)
+    {
+        this.apiKeyRepository = apiKeyRepository;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception
     {
-        return true;
+        String apiKey = request.getHeader(GeneralConstants.API_KEY);
+
+        if(StringUtils.isNotBlank(apiKey))
+        {
+            if(request.getRequestURI().startsWith("/api/v1/users"))
+            {
+                if(adminApiKey.equals(apiKey)) return true;
+            }
+            else
+            {
+                Optional<ApiKey> apiKeyOptional = apiKeyRepository.findByIdApiKey(apiKey);
+                if (apiKeyOptional.isPresent())
+                {
+                    request.setAttribute(GeneralConstants.USER, apiKeyOptional.get().getUser());
+                    return true;
+                }
+            }
+        }
+
+        throw new ResponseException(APIResponse.INVALID_API_KEY);
     }
 }
