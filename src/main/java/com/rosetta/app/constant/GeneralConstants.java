ed6d0a94677d41613a5bc31713f1cef90f4ccef2
Akshay Kumar <akshaykumarcnr@gmail.com>
2026-08-05 22:49:39 +0530

    Added payload constants.

--- src/main/java/com/rosetta/app/constant/GeneralConstants.java
+++ src/main/java/com/rosetta/app/constant/GeneralConstants.java
@@ -7,4 +7,8 @@ public class GeneralConstants
     public static final String PLAN = "plan";
     public static final String USER_ID = "user_id";
     public static final String API_KEYS = "api_keys";
+    public static final String SOURCE_TEXT = "source_text";
+    public static final String SOURCE_LANGUAGE = "source_language";
+    public static final String TRANSLATION_LANGUAGE = "translation_language";
+    public static final String TRANSLATION_ID = "translation_id";
 }
