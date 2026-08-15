02ba0a9fa9ba235f8aa0666b2be1da8812fdff8f
Akshay Kumar <akshaykumarcnr@gmail.com>
2026-08-15 23:21:12 +0530

    Optimized import.

--- src/main/java/com/rosetta/app/configuration/RedisConfig.java
+++ src/main/java/com/rosetta/app/configuration/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.rosetta.app.configuration;
 
-import com.rosetta.app.constant.ConfigConstants;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
